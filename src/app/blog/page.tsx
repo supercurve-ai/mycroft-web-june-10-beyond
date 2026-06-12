e256bb0d3ee2a036c79ce6bb1749dc707a236b12
Benji <33185282+BenjErgizerBunny@users.noreply.github.com>
2026-06-12 07:11:40 -0400

    fix

--- src/app/blog/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-/** The original site has no blog index: mycroft.io/blog 301s to /resources. */
-export default function BlogIndex() {
-  redirect("/resources");
-}
